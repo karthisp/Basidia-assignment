1f235e974acac44ab82da568fe0e8cd8a844d38e
Karthik SP <karthik.srinivasprasad@gmail.com>
2020-11-28 18:45:19 +0530

    weather api fix

--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -1,21 +1,24 @@
-(function(){
+import {weather} from './weather_api.js';
+
+(function () {
     const citySearch = document.querySelector('input');
     const displaySection = document.querySelector('.weather-display');
     const form = document.querySelector('form');
-    
+
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${citySearch.value}&days=3`, {
+        
+        fetch(weather.url(citySearch.value), {
             "method": "GET",
             "headers": {
-                "x-rapidapi-key": "d0a4bf049emsh661d05f14c34b3ap123e9cjsn7516c7c0cbfa",
-                "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+                "x-rapidapi-key": weather.apikey,
+                "x-rapidapi-host": weather.apiHost
             }
         })
             .then(response => response.json())
             .then(data => {
                 const dataPrint = {
-                    rain:data.forecast.forecastday[0].day.daily_chance_of_rain,
+                    rain: data.forecast.forecastday[0].day.daily_chance_of_rain,
                     city: data.location.name,
                     temp: data.current.temp_c,
                     feelslike: data.current.feelslike_c,
@@ -28,9 +31,9 @@
             .catch(err => {
                 console.log(err);
             });
-    
+
     })
-    
+
     function changeValues(values) {
         let city = document.querySelector('.city');
         let temp = document.querySelector('.temp');
@@ -39,7 +42,7 @@
         let wind = document.querySelector('.wind');
         let humidity = document.querySelector('.humidity');
         let uv = document.querySelector('.uv');
-    
+
         rain.innerText = values.rain
         city.innerText = values.city
         temp.innerText = values.temp
@@ -47,5 +50,5 @@
         wind.innerText = values.wind
         humidity.innerText = values.humidity
         uv.innerText = values.uv
-    } 
+    }
 })()
